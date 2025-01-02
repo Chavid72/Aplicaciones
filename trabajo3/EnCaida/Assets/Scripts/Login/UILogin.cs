421af646db000efa9e365d11a3eabcefc4f5011e
Nestor Bermejillo Gonzalez <n.bermejillo.2021@alumnos.urjc.es>
2025-01-02 16:48:02 +0100

    Login de google hecho

---
+++ trabajo3/EnCaida/Assets/Scripts/Login/UILogin.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Services.Authentication;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UILogin : MonoBehaviour
+{
+    [SerializeField] private Button loginButton;
+
+    [SerializeField] private TMP_Text userIDText;
+
+    [SerializeField] private Transform loginPanel, userPanel;
+
+    [SerializeField] private LoginController loginController;
+
+    private void OnEnable()
+    {
+        loginButton.onClick.AddListener(LoginButtonPressed);
+        loginController.OnSignedIn += LoginController_OnSignedIn;
+    }
+
+    private void LoginController_OnSignedIn(PlayerInfo playerInfo, string playerName)
+    {
+        loginPanel.gameObject.SetActive(false);
+        userPanel.gameObject.SetActive(true);
+
+        userIDText.text = $"id_{playerInfo.Id}";
+        Debug.Log(playerName);
+    }
+
+    private async void LoginButtonPressed()
+    {
+        await loginController.InitSignIn();
+    }
+
+    private void OnDisable()
+    {
+        loginButton.onClick.RemoveAllListeners();
+        loginController.OnSignedIn -= LoginController_OnSignedIn;
+    }
+}
