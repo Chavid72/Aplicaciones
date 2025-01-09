23d71c8e0e18d6dc997e762c2f370ad0fe8a46b2
Luis Javier Moreno Rojas <luisjamorevela@outlook.com>
2025-01-09 19:35:10 +0100

    Tienda, comprar cromas P1

--- trabajo3/EnCaida/Assets/Scripts/Tienda/Shop.cs
+++ trabajo3/EnCaida/Assets/Scripts/Tienda/Shop.cs
@@ -7,45 +7,63 @@ public class Shop : MonoBehaviour
 {
     public int monedas;
     public TMPro.TextMeshProUGUI monedasText;
+    public TMPro.TextMeshProUGUI mensajeText;
 
     public GameObject[] cromas;
-    public GameObject[] fondos;
-
+    public int[] preciosCromas;
+ 
     private int cromaActual = 0;
-    private int fondosActual = 0;
-    
+
+ 
     void Start()
     {
         monedas = PlayerPrefs.GetInt("Monedas", 0);
         cromaActual = PlayerPrefs.GetInt("Croma", 0);
-        fondosActual = PlayerPrefs.GetInt("Fondo", 0);
+        
 
         ActualizarInterfaz();
+        MostrarMensaje("");
     }
 
     public void ComprarMonedas(int cantidad)
     {
-        Debug.Log($"ComprarMonedas llamado con cantidad: {cantidad}");
         monedas += cantidad;
         GuardarDatos();
         ActualizarInterfaz();
+        MostrarMensaje($"Has comprado {cantidad} monedas.");
     }
 
-    public void SeleccionarCromaParacaidista(int index)
+    public void comprarCroma(int index)
+    {
+        if (index < cromas.Length && index < preciosCromas.Length)
+        {
+            int precio = preciosCromas[index];
+
+            if (monedas >= precio)
+            {
+                monedas -= precio;
+                cromaActual = index;
+
+                GuardarDatos();
+                ActualizarInterfaz();
+                MostrarMensaje($"¡Croma {index + 1} comprado por {precio} monedas!");
+            }
+            else 
+            {
+                MostrarMensaje("No tienes suficientes monedas para este croma.");
+            }
+        }
+        else
+        {
+            MostrarMensaje("Croma no válido.");
+        }
+    }
+
+    public void SeleccionarCroma(int index)
     {
         if (index < cromas.Length)
         {
             cromaActual = index;
-            GuardarDatos();
-            ActualizarInterfaz();
-        }
-    }
-
-    public void SeleccionarFondo(int index)
-    {
-        if(index < fondos.Length)
-        {
-            fondosActual = index;
             GuardarDatos();
             ActualizarInterfaz();
         }
@@ -56,11 +74,28 @@ public class Shop : MonoBehaviour
         monedasText.text = "Monedas: " + monedas;
     }
 
+    private void MostrarMensaje(string mensaje)
+    {
+        if (mensajeText != null)
+        {
+            mensajeText.text = mensaje;
+            CancelInvoke(nameof(LimpiarMensaje)); 
+            Invoke(nameof(LimpiarMensaje), 3f); 
+        }
+    }
+
+    private void LimpiarMensaje()
+    {
+        if (mensajeText != null)
+        {
+            mensajeText.text = "";
+        }
+    }
+
     private void GuardarDatos()
     {
         PlayerPrefs.SetInt("Monedas", monedas);
         PlayerPrefs.SetInt("Croma", cromaActual);
-        PlayerPrefs.SetInt("Fondo", fondosActual);
         PlayerPrefs.Save();
     }
 
